e88989dbd34bc9c09e059a14820cfbde8d2f98f0
juancego <juancego@unisabana.edu.co>
2025-02-14 08:30:53 -0500

    Correccion front

--- frontend/src/components/ListaNombres.js
+++ frontend/src/components/ListaNombres.js
@@ -1,20 +1,14 @@
 import React from "react";
 
 const ListaNombres = ({ nombres }) => {
-  console.log("📢 Nombres recibidos en ListaNombres:", nombres); // Verifica los datos
-
   return (
-    <div>
-      <h2>Lista de Nombres</h2>
-      <ul>
-        {nombres.length > 0 ? (
-          nombres.map((item, index) => <li key={item.id || index}>{item.nombre}</li>)
-        ) : (
-          <p>⏳ Cargando o no hay nombres disponibles...</p>
-        )}
-      </ul>
-    </div>
+    <ul>
+      {nombres.map((item, index) => (
+        <li key={index}>{item.nombre}</li>
+      ))}
+    </ul>
   );
 };
 
 export default ListaNombres;
+
