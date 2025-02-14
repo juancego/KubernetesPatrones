ecc8efd1a9aeeca6ff59e080471dde7de53f4e1a
juancego <juancego@unisabana.edu.co>
2025-02-14 07:54:16 -0500

    Prueba 2 de conexion a DB

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ async function testDBConnection() {
 }
 
 testDBConnection();
-/*
+
 
 const express = require("express");
 const cors = require("cors");
@@ -49,4 +49,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-*/
